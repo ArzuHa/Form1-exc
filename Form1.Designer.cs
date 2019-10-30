394511fddedbe1a38bf315f156c2a89f85dda917
ArzuHa <53233132+ArzuHa@users.noreply.github.com>
2019-10-30 19:23:27 +0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace FormExc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Ad = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxSurname = new System.Windows.Forms.TextBox();
+            this.BtnSend = new System.Windows.Forms.Button();
+            this.BtnSave = new System.Windows.Forms.Button();
+            this.BtnOpen = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Ad
+            // 
+            this.Ad.AutoSize = true;
+            this.Ad.Location = new System.Drawing.Point(12, 9);
+            this.Ad.Name = "Ad";
+            this.Ad.Size = new System.Drawing.Size(20, 13);
+            this.Ad.TabIndex = 0;
+            this.Ad.Text = "Ad";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Soyad";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(79, 6);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxName.TabIndex = 2;
+            // 
+            // textBoxSurname
+            // 
+            this.textBoxSurname.Location = new System.Drawing.Point(79, 51);
+            this.textBoxSurname.Name = "textBoxSurname";
+            this.textBoxSurname.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSurname.TabIndex = 3;
+            // 
+            // BtnSend
+            // 
+            this.BtnSend.Location = new System.Drawing.Point(240, 118);
+            this.BtnSend.Name = "BtnSend";
+            this.BtnSend.Size = new System.Drawing.Size(75, 23);
+            this.BtnSend.TabIndex = 4;
+            this.BtnSend.Text = "Send";
+            this.BtnSend.UseVisualStyleBackColor = true;
+            this.BtnSend.Click += new System.EventHandler(this.BtnSend_Click);
+            // 
+            // BtnSave
+            // 
+            this.BtnSave.Location = new System.Drawing.Point(240, 171);
+            this.BtnSave.Name = "BtnSave";
+            this.BtnSave.Size = new System.Drawing.Size(75, 23);
+            this.BtnSave.TabIndex = 5;
+            this.BtnSave.Text = "Save";
+            this.BtnSave.UseVisualStyleBackColor = true;
+            this.BtnSave.Click += new System.EventHandler(this.BtnSave_Click);
+            // 
+            // BtnOpen
+            // 
+            this.BtnOpen.Location = new System.Drawing.Point(240, 228);
+            this.BtnOpen.Name = "BtnOpen";
+            this.BtnOpen.Size = new System.Drawing.Size(75, 23);
+            this.BtnOpen.TabIndex = 6;
+            this.BtnOpen.Text = "Open";
+            this.BtnOpen.UseVisualStyleBackColor = true;
+            this.BtnOpen.Click += new System.EventHandler(this.BtnOpen_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // richTextBox
+            // 
+            this.richTextBox.Location = new System.Drawing.Point(2, 108);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.Size = new System.Drawing.Size(210, 174);
+            this.richTextBox.TabIndex = 7;
+            this.richTextBox.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox);
+            this.Controls.Add(this.BtnOpen);
+            this.Controls.Add(this.BtnSave);
+            this.Controls.Add(this.BtnSend);
+            this.Controls.Add(this.textBoxSurname);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Ad);
+            this.ForeColor = System.Drawing.Color.Red;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Ad;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxSurname;
+        private System.Windows.Forms.Button BtnSend;
+        private System.Windows.Forms.Button BtnSave;
+        private System.Windows.Forms.Button BtnOpen;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FormExc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSend_Click(object sender, EventArgs e)
+
+        {
+            if (string.IsNullOrWhiteSpace(textBoxName.Text))
+            {
+                errorProvider1.SetError(textBoxName, "Ad bos buraxila bilmez");
+            }
+
+            if (string.IsNullOrWhiteSpace(textBoxSurname.Text))
+            {
+                errorProvider1.SetError(textBoxSurname, "Soyad bos buraxila bilmez");
+            }
+
+            string name = textBoxName.Text;
+            string surname = textBoxSurname.Text;
+
+            richTextBox.Text+=($"{name} {surname}{Environment.NewLine}");
+
+            textBoxName.Clear();
+            textBoxSurname.Clear();
+        }
+
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+            string text1 = richTextBox.Text;
+            File.WriteAllText("Test.txt", text1);
+        }
+
+        private void BtnOpen_Click(object sender, EventArgs e)
+        {
+            richTextBox.Text = File.ReadAllText("Test.txt");
+
+        }
+    }
+
+    
+
+}
